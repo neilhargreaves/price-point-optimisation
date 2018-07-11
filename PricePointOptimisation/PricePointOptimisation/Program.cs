bca99385213942fd3ed4892b57b289aecbe0d967
Neil <neil.hargreaves@beautybay.com>
2018-07-11 13:49:48 +0100

    initial commit

---
+++ PricePointOptimisation/PricePointOptimisation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PricePointOptimisation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
